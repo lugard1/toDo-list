46ca9ee5143ff193110d666013e9b08aeda5d5e7
lugard1 <agulugard@gmail.com>
2022-11-11 13:40:56 +0100

    updated add.js task.js linters error checked

--- src/modules/add.js
+++ src/modules/add.js
@@ -1,19 +1,17 @@
 export default class Dom {
-  isChecked (c) {
-    if (c) {
-      return "checked";
-    } else {
-      return '';
-    }
- }
-
-
-  addDOM (taskArray) {
-    const taskArrayDOM = '';
+  static addDOM (taskArray) {
+    const isChecked = (c) => {
+      if (c) {
+        return " checked";
+      } else {
+        return '';
+      }
+   }
+    let taskArrayDOM = '';
     taskArray.forEach( (element) => {
-      taskArrayDOM = `<li><span><input type="checkbox" name="taskArray"`;
-      // taskArrayDOM += isChecked(element.completed);
-      taskArrayDOM += `<input class="input" type="text" name="taskArrayi" value="${element.description}"></span>`;
+      taskArrayDOM += `<li><span><input type="checkbox" name="taskArray"`;
+      taskArrayDOM += isChecked(element.completed);
+      taskArrayDOM += `><input class="input" type="text" name="taskArrayi" value="${element.description}"></span>`;
       taskArrayDOM += `<i class="fa-solid fa-ellipsis-vertical"></i></li><br>`;
     });
     return taskArrayDOM;
--- src/modules/taskArray.js
+++ src/modules/taskArray.js
@@ -1,17 +1,17 @@
 const taskArray = [
     {
       description: 'Complete  washing dishes',
-      completed: true,
+      completed: false,
       index: 0,
     },
     {
       description: 'Complete eating breakfast',
-      completed: true,
+      completed: false,
       index: 1,
     },
     {
       description: 'complete reading and woking ',
-      completed: true,
+      completed: false,
       index: 2,
     },
   ];
