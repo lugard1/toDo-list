3923dcdd73758fa1d00158611a6e194a748ccc6f
lugard1 <agulugard@gmail.com>
2022-11-14 12:48:25 +0100

    update on add.js

--- src/modules/add.js
+++ src/modules/add.js
@@ -8,7 +8,7 @@ export default class Dom {
     };
     let taskArrayDOM = '';
     taskArray.forEach((element) => {
-      taskArrayDOM += '<li><span><input type="checkbox" name="taskArray"';
+      taskArrayDOM += '<li><span><input class="checkbox" type="checkbox" name="taskArray"';
       taskArrayDOM += isChecked(element.completed);
       taskArrayDOM += `><input class="input" type="text" name="taskArrayi" value="${element.description}"></span>`;
       taskArrayDOM += '<i class="fa-solid fa-ellipsis-vertical"></i></li><br>';
