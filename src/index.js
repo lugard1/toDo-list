8688106c62a1eacc2d09f50260f2eb5ce62b7f5e
lugard1 <agulugard@gmail.com>
2022-11-18 13:35:25 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import Display from './modules/functionality.js';
 import './style.css';
 
-document.querySelector('.toDo-list').innerHTML = `
-<ul class="display-list">
-${Dom.addDOM(taskArray)}
-</ul>
-`;
+Display();
